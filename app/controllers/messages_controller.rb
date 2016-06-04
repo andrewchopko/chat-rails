4261bb440995d650b27461a13342588641800be1
Andrew <andreychopko@gmail.com>
2016-06-04 16:40:05 +0300

    Added messages feature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,44 @@
 class MessagesController < ApplicationController
+
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_chat
+  before_action :authenticate_user!
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    @message.chat_id = @chat.id
+
+    if @message.save
+      redirect_to @chat
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
+  end
+
+  def update
+    @message.update(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def set_chat
+    @chat = Chat.find(params[:chat_id])
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
   belongs_to :chats
+  belongs_to :user
 end
